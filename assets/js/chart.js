011f25a55c2368ed0f8c11f260841f8ea59c6010
chauhanpkc <chauhanprashant@ymail.com>
2023-04-18 15:27:27 +0530

    change

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,34 +1,63 @@
 // Chart
+// var options = {
+//   series: [{
+//   name: 'series1',
+//   data: [31, 40, 28, 51, 42, 109, 100]
+// }, {
+//   name: 'series2',
+//   data: [11, 32, 45, 32, 34, 52, 41]
+// }],
+//   chart: {
+//   height: 350,
+//   type: 'area',
+//   toolbar: {
+//     show: false
+//   },
+// },
+// dataLabels: {
+//   enabled: false
+// },
+// stroke: {
+//   curve: 'smooth'
+// },
+// xaxis: {
+//   type: 'datetime',
+//   categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+// },
+// tooltip: {
+//   x: {
+//     format: 'dd/MM/yy HH:mm'
+//   },
+// },
+// };
+
 var options = {
   series: [{
-  name: 'series1',
-  data: [31, 40, 28, 51, 42, 109, 100]
-}, {
-  name: 'series2',
-  data: [11, 32, 45, 32, 34, 52, 41]
+  data: [34, 44, 54, 21, 12, 43, 33, 23, 66, 66, 58],
 }],
   chart: {
+  type: 'line',
   height: 350,
-  type: 'area',
   toolbar: {
     show: false
   },
 },
+stroke: {
+  curve: 'stepline',
+  colors: ['#F44336']
+},
 dataLabels: {
   enabled: false
 },
-stroke: {
-  curve: 'smooth'
+title: {
+  text: 'Hostel',
+  align: 'left'
 },
-xaxis: {
-  type: 'datetime',
-  categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-},
-tooltip: {
-  x: {
-    format: 'dd/MM/yy HH:mm'
-  },
-},
+markers: {
+  hover: {
+    sizeOffset: 4
+  }
+}
 };
 
 var chart = new ApexCharts(document.querySelector("#chart"), options);
